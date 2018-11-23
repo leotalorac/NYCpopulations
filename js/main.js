c24cf53c59a7a219d8a23ba57b95d5d5e5c1389f
Luis Eduardo Otálora Cubides <leotalorac@unal.edu.co>
2018-11-23 11:40:34 -0500

    add markers

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 let map;
 let api = "https://data.cityofnewyork.us/resource/rreq-n6zk.json";
 let data = [];
+let markers = []
 let ck = Object.keys(coords)
 //get the data
 function getData(){
@@ -12,9 +13,25 @@ function getData(){
 				data.push(v);
 			}
 		});
+
 	});
-	
-	
+}
+//put the markers
+function putMarkers(){
+	console.log('test');
+	for(let i =0;i<ck.length;i++){
+		let center = coords[ck[i]];
+		console.log(center);
+		let marker = new google.maps.Marker({
+		    map:map,
+		    draggable:true,
+		    animation:google.maps.Animation.DROP,
+		    position: center,
+		    //icon: (BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE))
+	})                          
+		// To add the marker to the map, call setMap();
+		markers.push(marker)
+		}
 }
 
 //create the map
@@ -23,11 +40,12 @@ function initMap(){
 		//center of the map
           center: {lat: 40.7142700, lng: -74.0059700},
           // the zoom
-          zoom: 6,
+          zoom: 7,
           //remove the ui
           mapTypeId: google.maps.MapTypeId.ROADMAP,
           disableDefaultUI: true,
         });
+	putMarkers();
 }
 //show and hide the filters
 $('#show-button').click(() =>{
